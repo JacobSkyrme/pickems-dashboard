c86f5a679d7f7db2e2a86d58838648e6551633c3
Jacob Skyrme <skyrmejacob@gmail.com>
2023-11-06 17:26:27 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className='dark text-white'>
+      <head>
+      <meta name="google-adsense-account" content="ca-pub-2890852664383961"/>
+      </head>
       <body>
         <main className="min-h-screen bg-background flex flex-col items-center text-white">
           {children}
