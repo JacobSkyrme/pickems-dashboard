19bff409d5757b5ceffa5f7d530bdbbc972f99c9
Jacob Skyrme <skyrmejacob@gmail.com>
2024-09-27 00:47:59 +0100

    updated year

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import './globals.css'
 import Link from 'next/link'
 
 export const metadata = {
-  title: 'Worlds 2023 Pickems Tracker',
-  description: 'A dashboard keeping track of the League of Legends Worlds 2023 crystal ball and Pickems Trackers',
+  title: 'Worlds 2024 Pickems Tracker',
+  description: 'A dashboard keeping track of the League of Legends Worlds 2024 crystal ball and Pickems Trackers',
 }
 
 
@@ -17,6 +17,10 @@ export default function RootLayout({
       <head>
       </head>
       <body>
+        <nav>
+          <Link href="/">Home</Link>
+          <Link href="/worlds2024"></Link>
+        </nav>
         <main className="min-h-screen bg-background flex flex-col items-center text-white">
           {children}
         </main>
